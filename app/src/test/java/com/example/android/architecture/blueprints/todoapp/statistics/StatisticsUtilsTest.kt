b8d259edcd300c7cddd9e7a602172fb187a1a7cf
Angel Andrade Bergantiños <angel.bergantinos@accenture.com>
2022-05-04 14:53:21 +0200

    Testing basics [6], Step 1: Create a test class

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,12 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import org.junit.Assert.*
+
+import org.junit.After
+
+class StatisticsUtilsTest {
+
+    @After
+    fun tearDown() {
+    }
+}
