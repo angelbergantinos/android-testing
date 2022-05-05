8211d5155c5f97bcebb18d5dc4430f3872c8859c
Angel Andrade Bergantiños <angel.bergantinos@accenture.com>
2022-05-05 11:17:12 +0200

    Testing basics [7], Step 1: Write the tests

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -22,5 +22,40 @@ class StatisticsUtilsTest {
         assertThat(result.activeTasksPercent, `is`(100f))
         assertThat(result.completedTasksPercent, `is`(0f))
     }
+
+    @Test
+    fun given_task_list_when_one_completed_and_no_active_tasks_then_hundred_and_zero_percentage() {
+        // Create an active task
+        val tasks = listOf<Task>(
+                Task("The title", "Description of my task", isCompleted = true)
+        )
+
+        // Call your function
+        var result = getActiveAndCompletedStats(tasks)
+
+        // Check the result using hamcrest
+        assertThat(result.completedTasksPercent, `is`(100f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun given_task_list_when_two_completed_and_three_active_tasks_then_forty_and_sixty_percentage() {
+        // Create an active task
+        val tasks = listOf<Task>(
+                Task("The title", "Description of my task", isCompleted = true),
+                Task("The title 2", "Description of my task 2", isCompleted = false),
+                Task("The title 3", "Description of my task 3", isCompleted = true),
+                Task("The title 4", "Description of my task 4", isCompleted = false),
+                Task("The title 5", "Description of my task 5", isCompleted = false)
+        )
+
+        // Call your function
+        var result = getActiveAndCompletedStats(tasks)
+
+        // Check the result using hamcrest
+        assertThat(result.completedTasksPercent, `is`(40f))
+        assertThat(result.activeTasksPercent, `is`(60f))
+    }
+
 }
 
