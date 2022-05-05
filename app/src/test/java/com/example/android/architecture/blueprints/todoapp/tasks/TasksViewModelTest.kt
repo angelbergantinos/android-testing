2329d02fcabd631b4587b3a6e576d95862f2cea5
Angel Andrade Bergantiños <angel.bergantinos@accenture.com>
2022-05-05 13:50:11 +0200

    Testing basics [8], Step 2: Start Writing your ViewModel Test

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,5 +1,18 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
-import org.junit.Assert.*
+import org.junit.Test
 
-class TasksViewModelTest
+class TasksViewModelTest {
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+
+        // Given a fresh TasksViewModel
+
+        // When adding a new task
+
+        // Then the new task event is triggered
+
+    }
+
+}
